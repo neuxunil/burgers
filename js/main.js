fcdf75c2bc388f211d0196aa5d7d4a79225c375c
Sergey Olizarevich <neuxunil@yandex.ru>
2017-11-12 21:02:35 +0300

    Доработки

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     var menuVisible = $('.hamburger-menu-link'),
-        menuOff = $('.hamburger-menu__close'),
+        menuClose = $('.hamburger-menu__close'),
         menu = $('.hamburger-menu');
 
-    $(menuVisible).on('click touchstart', function (e) {
+    $(menuVisible).on('click', function(e) {
         e.preventDefault();
         $(menu).addClass('hamburger-menu_visible');
     });
 
-    $(menuOff).on('click touchstart', function (e) {
+    $(menuClose).on('click', function(e) {
         e.preventDefault();
         $(menu).toggleClass('hamburger-menu_visible');
     });
