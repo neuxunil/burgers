64d8587244a49851a0fd8fe77ae8a5081c7fafcf
Sergey Olizarevich <neuxunil@yandex.ru>
2017-11-23 19:01:48 +0300

    Доделан аккорден в секции меню

--- js/main.js
+++ js/main.js
@@ -37,6 +37,67 @@
         }
     });
 
+
+// Аккордеон в секции меню
+        const calculateWidth = () => {
+            const wWidth = $(window).width()
+            const titles = $('.menu-acco__trigger')
+            const titleWidth = titles.width()
+            const reqWidth = wWidth - (titleWidth * titles.length)
+
+            return (reqWidth > 550) ? 550 : reqWidth
+        }
+
+        const openItem = item => {
+            const container = $('.menu-acco')
+            const items = $('.menu-acco__item', container)
+            const accoText = $('.menu-acco__text', container)
+            const activeItem = items.filter('.active__menu')
+            const activeContent = activeItem.find('.menu-acco__content')
+            const content = item.find('.menu-acco__content')
+            const reqWidth = calculateWidth()
+
+            items.removeClass('active__menu');
+            item.addClass('active__menu');
+
+            accoText.hide();
+            activeContent.animate({ 'width': '0px' });
+
+            content.animate({
+                'width': reqWidth + 'px'
+            }, () => { accoText.fadeIn() })
+        }
+
+        const closeItem = item => {
+            item.removeClass('active__menu');
+
+            item.closest('.menu-acco').find('.menu-acco__text')
+                .stop(true, true).fadeOut(() => {
+                item.find('.menu-acco__content').animate({ 'width': '0px' });
+        });
+        }
+
+        $('.menu-acco__trigger').on('click', (e) => {
+            e.preventDefault();
+
+        const $this = $(e.target)
+        const item = $this.closest('.menu-acco__item')
+
+        item.hasClass('active__menu')
+            ? closeItem(item)
+            : openItem(item)
+    });
+
+        // клик вне аккордеона
+        $(document).on('click', (e) => {
+            const $this = $(e.target);
+
+        if (!$this.closest('.menu-acco').length) {
+            closeItem($('.menu-acco__item'))
+        }
+    });
+
+
     // Слайдер
     $(function() {
         var screen = $('.slider__list'),
